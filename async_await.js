8ea9670e4113dfd54d05cd6f923aebb5db060f02
hinhduong <duonghinh0402@gmail.com>
2022-09-27 16:41:28 +0700

    async await

---
+++ async_await.js
@@ -0,0 +1,69 @@
+function findRandomImgPromise(tag) {
+    const apiKey = '0UTRbFtkMxAplrohufYco5IY74U8hOes';
+    const endpoint = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=${tag}`;
+    return fetch(endpoint)
+      .then(rs => rs.json())
+      .then(json => json.data.images.original.url);
+  }
+  
+  //async/await
+  $('#request').click(async () => {
+    console.time('getAnimalSequential')
+
+    const catImgUrl = await findRandomImgPromise('cat');
+    const dogImgUrl = await findRandomImgPromise('dog');
+    const fishImgUrl = await findRandomImgPromise('fish');
+
+    console.timeEnd('getAnimalSequential')
+
+    $('#cat').attr('src', catImgUrl);
+    $('#dog').attr('src', dogImgUrl);
+    $('#fish').attr('src', fishImgUrl);
+  });
+  
+  // parallels
+//   $('#request').click(async () => {
+//       console.time('getAnimalParallels')
+
+//       const result = await Promise.all([
+//          findRandomImgPromise('cat'),
+//          findRandomImgPromise('dog'),
+//          findRandomImgPromise('fish')
+//       ]);
+
+//       console.timeEnd('getAnimalParallels')
+      
+//       console.table([result[0], result[0], result[2]])
+
+//       $('#cat').attr('src', result[0]);
+//       $('#dog').attr('src',result[0]);
+//       $('#fish').attr('src', result[2]);
+//     });
+  
+  
+    // loop
+  //   function setTimeoutPromise(time) {
+  //     return new Promise((resolve) => {
+  //       setTimeout(resolve, time);
+  //     });
+  //   }
+    
+  //   $('#request').click( () => {
+  //     // Asynchonous + Sequential
+  //     console.log('Begin run');
+  //     const firstLoop = randomImg('dog');
+  //     console.log('Dog run');
+  //     const secondLoop = randomImg('cat');
+  //     console.log('Cat run');
+      
+  //     await Promise.all([firstLoop, secondLoop]);
+  //     alert('All loop finished.');
+  //   });
+    
+  //   async function randomImg(tag) {
+  //     for(let i = 0; i < 5; i++) {
+  //       const imgUrl = await findRandomImgPromise(tag);
+  //       $('#' + tag).attr('src', imgUrl);
+  //       await setTimeoutPromise(2*1000);
+  //     }
+  //   }
